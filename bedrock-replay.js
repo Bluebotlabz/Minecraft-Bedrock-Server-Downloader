b4261bf986371bbbcc334a5b839f20d694bf209d
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-10-16 11:37:52 +0100

    It actually works now
    - Implement subchunk sending
    - Load data from json files instead of hardcoding it
    - Create python file to generate needed subchunk and chunk files
    - Improve python converter

--- bedrock-replay.js
+++ bedrock-replay.js
@@ -1,18 +1,15 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const bedrock = require('bedrock-protocol')
+//const { join } = require('path')
 
 var serverProperties;
 var worldChunks;
 var biomeDefinitions;
 var entityIdentifiers;
 
-async function loadData() {
-  serverProperties = JSON.parse(await fs.readFile("./properties.json", { encoding: 'utf8' }))
-  worldChunks = JSON.parse(await fs.readFile("./chunkData.json", { encoding: 'utf8' }))
-  biomeDefinitions = JSON.parse(await fs.readFile("./biomeDefinitions.json", { encoding: 'utf8' }))
-  entityIdentifiers = JSON.parse(await fs.readFile("./entityIdentifiers.json", { encoding: 'utf8' }))
-  playerList = JSON.parse(await fs.readFile("./playerList.json", { encoding: 'utf8' }))
-}
+//const getPath = (packetPath) => join(__dirname, `../data/${server.options.version}/${packetPath}`)
+//const get = (packetName) => require(getPath(`./data/${packetName}.json`))
+const get = (packetName) => require(`./data/${packetName}.json`)
 
 const server = bedrock.createServer({
   host: '0.0.0.0',       // optional. host to bind as.
@@ -21,11 +18,13 @@ const server = bedrock.createServer({
 })
 
 console.log("Loading files...")
-clientData = {"position": {"x": 0, "y": 0, "z": 0}}
+const respawnPacket = get('respawn')
 
-loadData().then(() => {
-  console.log("Server ready.");
-})
+//loadData().then(() => {
+//  console.log("Server ready.");
+//})
+
+console.log("Server ready.");
 
 server.on('connect', client => {
   client.on('join', () => { // The client has joined the server.
@@ -45,58 +44,105 @@ server.on('connect', client => {
     // Resource pack response
     client.on('resource_pack_client_response', (data) => {
       if (data.response_status === 'have_all_packs') {
+        //client.write('network_settings', { compression_threshold: 1 })
         client.queue("resource_pack_stack", {"must_accept":true,"behavior_packs":[],"resource_packs":[{"uuid":"3bdebb27-13ad-6aa7-b726-e703c4b3fe28","version":"1.0.47","name":""}],"game_version":"*","experiments":[{"name":"spectator_mode","enabled":true},{"name":"data_driven_items","enabled":true}],"experiments_previously_used":true})
       } else if (data.response_status === 'completed') {
         // Client ready
         console.log("client ready");
 
-        client.queue("start_game", serverProperties)
-        client.queue("set_time", {"time": 73000})
-        client.queue("set_spawn_position", {"spawn_type":"player","player_position":{"x":-2147483648,"y":-2147483648,"z":-2147483648},"dimension":3,"world_position":{"x":-2147483648,"y":-2147483648,"z":-2147483648}})
-        client.queue("set_difficulty", {"difficulty": 1})
-        client.queue("set_commands_enabled", {"enabled": true})
-        client.queue("set_time", {"time": 73000})
-        client.queue("update_adventure_settings", {"no_pvm":false,"no_mvp":false,"immutable_world":false,"show_name_tags":false,"auto_jump":true})
-        client.queue("game_rules_changed", {"rules":[{"name":"commandblockoutput","editable":true,"type":"bool","value":true},{"name":"dodaylightcycle","editable":true,"type":"bool","value":false},{"name":"doentitydrops","editable":true,"type":"bool","value":true},{"name":"dofiretick","editable":true,"type":"bool","value":true},{"name":"domobloot","editable":true,"type":"bool","value":false},{"name":"domobspawning","editable":true,"type":"bool","value":false},{"name":"dotiledrops","editable":true,"type":"bool","value":true},{"name":"doweathercycle","editable":true,"type":"bool","value":false},{"name":"drowningdamage","editable":true,"type":"bool","value":true},{"name":"falldamage","editable":true,"type":"bool","value":false},{"name":"firedamage","editable":true,"type":"bool","value":true},{"name":"keepinventory","editable":true,"type":"bool","value":true},{"name":"mobgriefing","editable":true,"type":"bool","value":false},{"name":"pvp","editable":true,"type":"bool","value":false},{"name":"showcoordinates","editable":true,"type":"bool","value":false},{"name":"naturalregeneration","editable":true,"type":"bool","value":false},{"name":"tntexplodes","editable":true,"type":"bool","value":true},{"name":"sendcommandfeedback","editable":true,"type":"bool","value":false},{"name":"maxcommandchainlength","editable":true,"type":"int","value":65536},{"name":"doinsomnia","editable":true,"type":"bool","value":true},{"name":"commandblocksenabled","editable":true,"type":"bool","value":false},{"name":"randomtickspeed","editable":true,"type":"int","value":0},{"name":"doimmediaterespawn","editable":true,"type":"bool","value":true},{"name":"showdeathmessages","editable":true,"type":"bool","value":false},{"name":"functioncommandlimit","editable":true,"type":"int","value":10000},{"name":"spawnradius","editable":true,"type":"int","value":10},{"name":"showtags","editable":true,"type":"bool","value":true},{"name":"freezedamage","editable":true,"type":"bool","value":true},{"name":"respawnblocksexplode","editable":true,"type":"bool","value":true},{"name":"showbordereffect","editable":true,"type":"bool","value":true}]})
-        client.queue("player_list", playerList)
-        client.queue("biome_definition_list", biomeDefinitions)
-        client.queue("available_entity_identifiers", entityIdentifiers)
-        client.queue("player_fog", {"stack": []})
-        client.queue("set_health", {"health": 20})
-        client.queue("play_status", {"status":"player_spawn"})
+        client.queue('player_list', get('player_list'))
+        client.queue('start_game', get('start_game'))
+        client.queue('item_component', { entries: [] })
+        client.queue('set_spawn_position', get('set_spawn_position'))
+        client.queue('set_time', get('set_time'))
+        client.queue('set_difficulty', { difficulty: 1 })
+        client.queue('set_commands_enabled', { enabled: true })
+        client.queue('update_adventure_settings', get('update_adventure_settings'))
+        client.queue('biome_definition_list', get('biome_definition_list'))
+        client.queue('available_entity_identifiers', get('available_entity_identifiers'))
+        client.queue('update_attributes', get('update_attributes'))
+        client.queue('creative_content', get('creative_content'))
+        client.queue('inventory_content', get('inventory_content'))
+        client.queue('player_hotbar', { selected_slot: 3, window_id: 'inventory', select_slot: true })
+        client.queue('crafting_data', get('crafting_data'))
+        client.queue('available_commands', get('available_commands'))
+        //client.queue('chunk_radius_update', { chunk_radius: 1 })
+        client.queue('game_rules_changed', get('game_rules_changed'))
+        client.queue('respawn', get('respawn'))
 
 
-        client.queue("network_chunk_publisher_update", {"coordinates":{"x":18,"y":25,"z":-39},"radius":64,"saved_chunks":[]})
+        //client.queue("network_chunk_publisher_update", {"coordinates":{"x":18,"y":25,"z":-39},"radius":64,"saved_chunks":[]})
 
         // Send all the chunks, idc how many u want, client
-        console.log("sending chunks")
-        for (var i = 0; i < worldChunks.length; i++) {
-          client.queue("level_chunk", worldChunks[i])
+        for (const file of fs.readdirSync(`./chunks`)) {
+          // I'm sorry HDD...
+          const chunkData = JSON.parse(fs.readFileSync(`./chunks/` + file))
+          // console.log('Sending chunk', buffer)
+          //client.sendBuffer(buffer)
+
+          //console.log(file)
+
+          client.queue("level_chunk", chunkData)
+        }
+
+        // Constantly send this packet to the client to tell it the center position for chunks. The client should then request these
+        // missing chunks from the us if it's missing any within the radius. `radius` is in blocks.
+        loop = setInterval(() => {
+          console.log("Sending network chonk")
+          client.write('network_chunk_publisher_update', { coordinates: { x: respawnPacket.position.x, y: 47, z: respawnPacket.position.z }, radius: 160,"saved_chunks":[] })
+        }, 4500)
+
+        // Wait some time to allow for the client to recieve and load all the chunks
+        setTimeout(() => {
+          // Allow the client to spawn
+          client.write('play_status', { status: 'player_spawn' })
+        }, 6000)
+      }
+    })
+
+    //client.on('subchunk_request', (data) => {
+    //  // Send all the chunks, idc how many u want, client
+    //  console.log("sending chunk data")
+//
+    //  client.queue("network_chunk_publisher_update", {"coordinates":{"x":Math.round(clientData.position.x),"y":Math.round(clientData.position.x),"z":Math.round(clientData.position.x)},"radius":64,"saved_chunks":[]})
+    //  
+    //  for (var i = 0; i < worldChunks.length; i++) {
+    //    client.queue("level_chunk", worldChunks[i])
+    //  }
+    //})
+
+    client.on("subchunk_request", (data) => {
+      console.log("subchunk request:", data.origin)
+
+      for (const file of fs.readdirSync(`./subchunks`)) {
+        // I'm sorry HDD...
+        const chunkData = JSON.parse(fs.readFileSync(`./subchunks/` + file))
+        // console.log('Sending chunk', buffer)
+        //client.sendBuffer(buffer)
+
+        //console.log(file)
+
+        //console.log(chunkData.origin)
+
+        if (chunkData.origin.x === data.origin.x && chunkData.origin.y === data.origin.y && chunkData.origin.z === data.origin.z) {
+          console.log("\n\n\nSending",chunkData.origin,"\n\n\n")
+          client.queue("subchunk", chunkData)
         }
       }
     })
 
-    client.on('subchunk_request', (data) => {
-      // Send all the chunks, idc how many u want, client
-      console.log("sending chunk data")
+    //client.on('move_player', (data) => {
+    //  // What is anticheat lol
+    //  client.queue("move_player", data)
+    //})
 
-      client.queue("network_chunk_publisher_update", {"coordinates":{"x":Math.round(clientData.position.x),"y":Math.round(clientData.position.x),"z":Math.round(clientData.position.x)},"radius":64,"saved_chunks":[]})
-      
-      for (var i = 0; i < worldChunks.length; i++) {
-        client.queue("level_chunk", worldChunks[i])
-      }
-    })
-
-    client.on('move_player', (data) => {
-      // What is anticheat lol
-      client.queue("move_player", data)
-
-      clientData.position = data.position;
-    })
-
-    client.on("tick_sync", (data) => {
-      // This will definitely break something
-      client.queue("tick_sync", data)
+    // Respond to tick synchronization packets
+    client.on('tick_sync', (packet) => {
+      console.log("TICKEROOOOOO")
+      client.queue('tick_sync', {
+        request_time: packet.request_time,
+        response_time: BigInt(Date.now())
+      })
     })
 
     //client.on('resource_pack_client_response')
--- network-to-world/csv2json.py
+++ network-to-world/csv2json.py
@@ -1,20 +1,31 @@
-linesToKeep = [
-    "clientbound,network_chunk_publisher_update,",
-    "clientbound,level_chunk,",
-    "serverbound,subchunk_request,",
+#ignorePackets = [
+#    "network_chunk_publisher_update,",
+#    "level_chunk,",
+#    "subchunk_request,",
+#]
+
+from yaml import parse
+
+
+ignorePackets = []
+
+packetValidation = [
+    "clientbound",
 ]
+
 #    "serverbound,move_player,"
 #]
 
-with open("./convertedData.csv", 'w+') as file:
-    file.write("reciptient,name,json\n")
+#with open("./convertedData.csv", 'w+') as file:
+#    file.write("reciptient,name,json\n")
 
 with open("./networkData.csv") as file:
     for line in file:
-        for lineToKeep in linesToKeep:
-            if (lineToKeep in line):
-                with open("./convertedData.csv", 'a') as otherFile:
-                    otherFile.write(line)
-                
-                continue
+        line = line.split(",")
+        if (line[0] in packetValidation and not line[1] in ignorePackets):
+            with open ("./data/" + line[1] + ".json", 'w+') as packetFile:
+                parsedLine = ','.join(line[2:])
+                parsedLine = parsedLine.replace('""', '"')
+                parsedLine = parsedLine[1:-2] # Remove the outermost "s
 
+                packetFile.write(parsedLine)
