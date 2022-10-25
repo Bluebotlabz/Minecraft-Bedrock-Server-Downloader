741199e54b1dcb1c96d92a3e909c68348ed95091
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-10-25 19:46:45 +0100

    Add block runtime id file

---
+++ blockRuntimeIDs.js
@@ -0,0 +1,19 @@
+const blockRuntimeIDs = {
+    // Lever
+    leverOffNorth: 6569,
+    leverOnNorth: 6561,
+    leverOffEast: 6562,
+    leverOnEast: 6570,
+    leverOffSouth: 6561,
+    leverOnSouth: 6569,
+    leverOffWest: 6570,
+    leverOnWest: 6562,
+
+    // Misc. Blocks
+    air: 60,
+
+    // Glass
+    glass: 6206
+}
+
+module.exports = blockRuntimeIDs
